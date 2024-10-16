e26bef62dbe1925bd557eed3c368cc1f9ff97ad2
Ilnur Abdullin <ilnur6005@gmail.com>
2024-10-16 14:10:48 +0400

    Add assertion

--- test_items.py
+++ test_items.py
@@ -1,6 +1,17 @@
+import time
+
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 
 
 def test_guest_should_see_button_adding_items_in_card(browser):
     browser.get(f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+
+    try:
+        button = browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    except NoSuchElementException:
+        button = None
+
+    assert button is not None, "Кнопка добавления товара в корзину не найдена"
+
+    time.sleep(2)
